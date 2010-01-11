2963dc4169480e2f530466a0e26425bcaf0c8f0f
Steen Manniche <steen@manniche.net>
2010-01-11 23:49:47 +0100

    script to do the cumbersome process of starting a RMI server correctly.

---
+++ scripts/rmi_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import sys
+
+classes   = '../build/classes'
+classpath = '%s:../'%( classes )
+rmiclass  = 'net.manniche.orep.server.RMIObjectRepository'
+rmibase   = 'java.rmi.server.codebase=file:///%s'%( os.path.abspath( classes )+'/' )
+
+#first check that the rmiregistry is running
+res = subprocess.Popen( [ 'ps a | grep rmiregistry' ], shell=True, stdout=subprocess.PIPE )
+
+res_list = [ process.partition( 'pts' )[0].strip() for process in res.communicate()[0].split( '\n' ) if process.find( 'grep rmiregistry' ) < 0 ]
+
+res_list = [ i for i in res_list if len(i)>0 ]
+
+if len( res_list ) > 1:
+    sys.exit( 'cannot automatically determine whether rmiregistry is running. Could you please find out for me?' )
+
+java_cmd = 'java -cp %s'%( classpath )+' -D'+rmibase+' '+rmiclass
+
+java = [ java_cmd ]
+
+java_proc = subprocess.Popen( java, shell=True, stdout=subprocess.PIPE )
+
+out = java_proc.communicate()[0]
+print out
+
