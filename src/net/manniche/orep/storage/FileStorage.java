8254cd4a067a0f1938bbb41e07a25a1f4f197fb4
Steen Manniche <steen@manniche.net>
2010-01-12 20:25:50 +0100

    finished implementation of filebased storage method

--- src/net/manniche/orep/storage/FileStorage.java
+++ src/net/manniche/orep/storage/FileStorage.java
@@ -21,6 +21,7 @@ import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.util.List;
@@ -33,13 +34,29 @@ import java.util.List;
 public class FileStorage implements StorageProvider
 {
 
-    private final File storage_file;
+    private static final Logger log = Logger.getLogger( FileStorage.class.getName() );
+    private final String storage_path;
 
     public FileStorage()
     {
-        String storage_path = System.getProperty( "user.home" ) + System.getProperty( "file.separator" ) + "objectstorage";
-        System.out.println( String.format( "%s", storage_path ) );
-        this.storage_file = new File( storage_path );
+        String sep = System.getProperty( "file.separator" );
+        this.storage_path = System.getProperty( "user.home" ) + sep + "objectstorage" + sep;
+
+        // ensure that the folder exists:
+        File storage_dir = new File( this.storage_path );
+
+        if( ! storage_dir.exists() )
+        {
+            log.info( String.format( "%s does not exist, creating it", this.storage_path ) );
+            boolean could_create_dirs = storage_dir.mkdirs();
+            if( ! could_create_dirs )
+            {
+                String error = String.format( "Could not create dir: %s, Please check permissions and disk space", this.storage_path );
+                log.severe( error );
+                throw new IllegalStateException( error );
+            }
+        }
+        log.info( String.format( "Storing files at %s", this.storage_path ) );
     }
 
 
@@ -53,18 +70,40 @@ public class FileStorage implements StorageProvider
             hash = hash.substring( 1 );
         }
 
-        String path = new Long( System.currentTimeMillis() ).toString() + hash;
+        String fname = new Long( System.currentTimeMillis() ).toString() + hash;
+        String store = this.storage_path + fname;
 
-        URI id = null;
-        try
+        URI id = this.generateURI( store );
+
+        File f = new File( id );
+
+        while( f.exists() )
+            // this should never happen, but if it does, we should only need to
+            // this once...
         {
-            id = new URI( "file", null, path, null );
+            fname = new Long( System.currentTimeMillis() ).toString() + hash;
+            store = this.storage_path + fname;
+            id = this.generateURI( store );
+            f = new File( id );
         }
-        catch( URISyntaxException ex )
+
+        log.info( String.format( "Abs filename: %s", store ) );
+
+        final FileOutputStream fos = new FileOutputStream( f, false );
+        fos.write( object );
+        fos.flush();
+        fos.close();
+
+        if( ! f.exists() )
         {
-            String error = String.format( "Could not construct storage location: %s", ex.getMessage() );
-            Logger.getLogger( FileStorage.class.getName() ).log( Level.SEVERE, error, ex );
-            throw new IOException( error, ex );
+            /**
+             * todo: This is really just sarcastic. I'm not entirely sure that
+             * end users or clients would appreciate this message from their
+             * low-level storage implementation.
+             */
+            String warn = String.format( "The file %s was not written to disk, and I have no further information on it's wereabouts", f.getAbsolutePath() );
+            log.warning( warn );
+            id = this.generateURI( "/dev/null" );
         }
 
         return id;
@@ -75,6 +114,23 @@ public class FileStorage implements StorageProvider
     public void save( byte[] object, URI uri ) throws IOException
     {
         throw new UnsupportedOperationException( "Not supported yet." );
+    }
+
+
+    private URI generateURI( String abs_path ) throws IOException
+    {
+        URI id = null;
+        try
+        {
+            id = new URI( "file", null, abs_path, null );
+        }
+        catch( URISyntaxException ex )
+        {
+            String error = String.format( "Could not construct storage location: %s", ex.getMessage() );
+            log.log( Level.SEVERE, error, ex );
+            throw new IOException( error, ex );
+        }
+        return id;
     }
 
 
@@ -104,4 +160,6 @@ public class FileStorage implements StorageProvider
     {
         throw new UnsupportedOperationException( "Not supported yet." );
     }
+
+
 }
