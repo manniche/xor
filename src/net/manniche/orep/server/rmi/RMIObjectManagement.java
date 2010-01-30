a5c242674595159a5ae60caafe68102f0fe5189b
Steen Manniche <steen@manniche.net>
2010-01-30 22:48:23 +0100

    Added up to date documentation

--- src/net/manniche/orep/server/rmi/RMIObjectManagement.java
+++ src/net/manniche/orep/server/rmi/RMIObjectManagement.java
@@ -2,18 +2,18 @@
  *  This file is part of OREP
  *  Copyright © 2009, Steen Manniche.
  * 
- *  RMIObjectRepository is free software: you can redistribute it and/or modify
+ *  OREP is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  * 
- *  RMIObjectRepositoryis distributed in the hope that it will be useful,
+ *  OREP is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  * 
  *  You should have received a copy of the GNU General Public License
- *  along with RMIObjectRepository.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with OREP.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package net.manniche.orep.server.rmi;
@@ -26,11 +26,12 @@ import net.manniche.orep.types.ObjectIdentifier;
 
 
 /**
- * Interface that defines the RMI operations that can be invoked. To ease the use
- * for Implementors, they are encouraged to use the methods defined in this
- * interface as wrappers for the methods defined in the {@link ObjectRepository}
- * interface, that defines the general operations for the object repository.
- * This behaviour is exemplified in the {@link RMIObjectRepository} implementation.
+ * Interface that defines the RMI operations that can be invoked. For RMI 
+ * operations, the  methods defined in this interface are used as  wrappers for
+ * the methods defined in the {@link ObjectRepository} interface, which in turn
+ * defines the general operations for the object repository.
+ * An example implementation of a (naive) RMI client can be found in the
+ * {@link RMIObjectRepository} implementation.
  * @author stm
  */
 public interface RMIObjectManagement extends ObjectRepository, Remote{
