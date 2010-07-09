5dbaa1e7c01f4ef816d9534c29421c9f0c9c32ed
Steen Manniche <steen@manniche.net>
2010-07-09 14:54:36 +0200

    helper type used for returning objects from the core server

---
+++ src/main/java/net/manniche/xor/types/InternalDigitalObject.java
@@ -0,0 +1,58 @@
+/*
+ *  This file is part of OREP
+ *  Copyright © 2009, Steen Manniche.
+ *
+ *  OREP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  OREP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OREP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.manniche.xor.types;
+
+import java.io.Serializable;
+
+
+/**
+ * Default implementation of the {@link net.manniche.xor.types.DigitalObject}
+ * This type only supports the most basic operations, including and limited to
+ * storing the data contents and defining the
+ * {@link net.manniche.xor.types.ObjectRepositoryContentType content type} of
+ * the data.
+ *
+ * The DefaultDigitalObject is an immutable type and thereby completely thread-
+ * safe.
+ *
+ * @author stm
+ */
+public final class InternalDigitalObject implements DigitalObject, Serializable{
+    static final long serialVersionUID = 4558861702889722277L;
+
+    private final byte[] internal_input;
+//    private final ObjectRepositoryContentType contentType;
+
+    public InternalDigitalObject( byte[] input ) //, ObjectRepositoryContentType contentType )
+    {
+        this.internal_input = input;
+//        this.contentType = contentType;
+    }
+    @Override
+    public byte[] getBytes()
+    {
+        return this.internal_input;
+    }
+
+//    @Override
+//    public ObjectRepositoryContentType getContentType()
+//    {
+//        return this.contentType;
+//    }
+}
