89356949b361a708dbc68379780140bc055c5128
Steen Manniche <steen@manniche.net>
2010-07-10 08:57:27 +0200

    adding license

--- src/main/java/net/manniche/xor/metadata/DublinCore.java
+++ src/main/java/net/manniche/xor/metadata/DublinCore.java
@@ -1,25 +1,21 @@
 /*
-This file is part of opensearch.
-Copyright © 2009, Dansk Bibliotekscenter a/s,
-Tempovej 7-11, DK-2750 Ballerup, Denmark. CVR: 15149043
-
-opensearch is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-opensearch is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with opensearch.  If not, see <http://www.gnu.org/licenses/>.
+ *  This file is part of xor
+ *  Copyright © 2009, Steen Manniche.
+ * 
+ *  xor is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  xor is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with xor.  If not, see <http://www.gnu.org/licenses/>.
  */
-/**
- * \file
- * \brief
- */
+
 package net.manniche.xor.metadata;
 
 import java.io.ByteArrayOutputStream;
